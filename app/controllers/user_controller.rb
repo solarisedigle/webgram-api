f67c714e802b60fcbeb986f5019b203b8a604276
solarisedigle <vitalik.shumanskyi@gmail.com>
2021-05-10 23:35:32 +0300

    Remove password ignoring (Should fix the bug)

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,8 @@
-require 'digest'
 class UserController < ApplicationController
     def register
         user = User.new
         user.username = params[:username]
-        user.password = params[:password]
+        user.password = params[:password].nil? ? '' : Digest::SHA2.hexdigest(params[:username] + @secret_key + params[:password])
         user.description = params[:description]
         user.activated = 0
         user.role = 'user'
@@ -17,10 +16,24 @@ class UserController < ApplicationController
     def print_all
         render json: User.all
     end
+    def view
+        user = User.where(id: params[:id])
+        if user.length > 0
+            result = {
+                :username => user[0]["username"],
+                :description => user[0]["description"],
+                :last_action => user[0]["last_action"],
+                :role => user[0]["role"],
+            }
+        else
+            result = {:error => "Fatal error: User " + params[:id] + " not found"}
+        end
+        render json: result
+    end
     def login
         result = {}
         if(!params[:username].nil? && !params[:password].nil?)
-            user = User.where(username: params[:username], password: params[:password])
+            user = User.where(username: params[:username], password: Digest::SHA2.hexdigest(params[:username] + @secret_key + params[:password]))
             if user.length > 0
                 user = user[0]
                 token = JWT.encode({user: user.id, exp: Time.now.to_i + 300}, @secret_key, 'HS256')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    validates :username, uniqueness: { case_sensitive: false }, format: { with: /\A^(?=.{8,30}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$\z/, message: "8-30 characters. Only [A-Z], [a-z] and delimiters [.] [_] are allowed" }
+    validates :username, uniqueness: { case_sensitive: false }, format: { with: /\A^(?=.{5,30}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$\z/, message: "5-30 characters. Only [A-Z], [a-z] and delimiters [.] [_] are allowed" }
     validates :password, presence: true
-    self.ignored_columns = %w[password]
 end
