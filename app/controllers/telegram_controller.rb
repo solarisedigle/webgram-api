b9efea83d903e80dafd166d6a0bc1c8e0ce751b3
solarisedigle <vitalik.shumanskyi@gmail.com>
2021-05-11 14:28:19 +0300

    DEBUG #3

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -1,13 +1,12 @@
 class TelegramController < ApplicationController
     def send_message(method_name, param_data)
         apikey = '1823283766:AAEou7GuSPE5dhWfDpOWKT-jT_-uMuSqAMw'
-        return HTTParty.get("https://api.telegram.org/bot" + apikey + "/" + method_name,
-            :body => param_data)
+        #return HTTParty.get("https://api.telegram.org/bot" + apikey + "/" + method_name,:body => param_data)
     end
     def index
-        #if params[:secret] != @secret_key
-        #    render(json: {:error => {:code => 401, :text => "Secret key is not valid"}}) and return
-        #end
+        if params[:secret] != @secret_key
+            render(json: {:error => {:code => 401, :text => "Secret key is not valid"}}) and return
+        end
         puts params.to_json
         puts send_message('sendMessage', {:chat_id => 418289311, :text => params.to_json, :parse_mode => 'HTML'}).response.body
     end
