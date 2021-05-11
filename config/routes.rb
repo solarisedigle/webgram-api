beafe9eaea49d32eee04c90c8bb1925d50be45dc
solarisedigle <vitalik.shumanskyi@gmail.com>
2021-05-11 14:10:36 +0300

    Telegram API test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'test#index'
   get 'api/v1/p_headers', to: 'test#p_headers'
-  get 'api/v1/login', to: 'user#login'
+  post 'api/v1/login', to: 'user#login'
   get 'api/v1/user', to: 'user#print_all'
-  get 'api/v1/user/new', to: 'user#register'
+  post 'api/v1/user/new', to: 'user#register'
   get 'api/v1/user/:id', to: 'user#view'
+  post 'api/telegramHandler/:secret', to: 'telegram#index'
 end
